c87c4c8c82dc3ada05a9b5e86ab5006d728ecf74
Aulad Hossen <himelaulad3021@gmail.com>
2023-07-23 15:25:12 +0600

    final commit

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,45 +1,48 @@
 <script setup>
-import { reactive, computed } from 'vue'
+import { reactive, computed, onMounted, onBeforeUnmount } from 'vue'
 
 const state = reactive({
     images:[
         {
             thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-            full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+            url: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
             name: 'image-01'
         },
         {
-          thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
-          full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
-          name: 'image-02'
+            thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
+            url: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+            name: 'image-02'
         },
         {
-          thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-          full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
-          name: 'image-03'
+            thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+            url: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+            name: 'image-03'
+        },  
+        {  
+            thumb: "https://images.unsplash.com/photo-1688841534418-b2e6ca99985a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=60",
+            url: "https://images.unsplash.com/photo-1688841534418-b2e6ca99985a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=60",
+            name: 'image-05'
+        },  
+        {  
+            thumb: "https://images.unsplash.com/photo-1688118935892-b40fbf3b5e69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
+            url: "https://images.unsplash.com/photo-1688118935892-b40fbf3b5e69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
+            name: 'image-06'
+        },  
+        {  
+            thumb: "https://images.unsplash.com/photo-1688153453932-bf45fe9d5f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
+            url: "https://images.unsplash.com/photo-1688153453932-bf45fe9d5f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
+            name: 'image-07'
         },
         {
-          thumb: "https://images.unsplash.com/photo-1688841534418-b2e6ca99985a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=60",
-          full: "https://images.unsplash.com/photo-1688841534418-b2e6ca99985a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=60",
-          name: 'image-05'
-        },
-        {
-          thumb: "https://images.unsplash.com/photo-1688118935892-b40fbf3b5e69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
-          full: "https://images.unsplash.com/photo-1688118935892-b40fbf3b5e69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
-          name: 'image-06'
-        },
-        {
-          thumb: "https://images.unsplash.com/photo-1688153453932-bf45fe9d5f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
-          full: "https://images.unsplash.com/photo-1688153453932-bf45fe9d5f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
-          name: 'image-07'
-        },
-        {
-          thumb: "https://images.unsplash.com/photo-1688232543149-5602b136ba11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
-          full: "https://images.unsplash.com/photo-1688232543149-5602b136ba11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
-          name: 'image-08'
+            thumb: "https://images.unsplash.com/photo-1688232543149-5602b136ba11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=80",
+            url: "https://images.unsplash.com/photo-1688232543149-5602b136ba11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80",
+            name: 'image-08'
         },
     ],
     currentIndex: 0,
+    intervalId: null,
+    autoSlide: true,
+    slideInterval: 5000,
 });
 
 const currentImage = computed(() => state.images[state.currentIndex]);
@@ -55,22 +58,55 @@ const previous = () => {
 const goToCurrentSlide = (index) => {
       state.currentIndex = index;
 };
+
+
+const startAutoSlide = () => {
+    state.intervalId = setInterval(() => {
+        if (state.autoSlide) {
+          next();
+        }
+    }, state.slideInterval);
+};
+
+const stopAutoSlide = () => {
+    clearInterval(state.intervalId);
+};
+
+const pauseAutoSlide = () => {
+    stopAutoSlide();
+};
+
+const resumeAutoSlide = () => {
+    if (state.autoSlide) {
+        startAutoSlide();
+    }
+};
+
+onMounted(() => {
+    startAutoSlide();
+});
+
+onBeforeUnmount(() => {
+    stopAutoSlide();
+});
+
 </script>
 
 <template>
     <p class="text-2xl font-bold my-4">Image Carousel</p>
-    <div class="image-slider flex flex-col p-3 border border-black">
+    <div class="image-slider flex flex-col border">
         <section class="flex justify-center items-center overflow-hidden mt-5">
-            <button @click="previous"><span class="material-symbols-outlined">chevron_left</span></button>
+            <button class="button" @click="previous" @mouseenter="pauseAutoSlide" @mouseleave="resumeAutoSlide"><span class="material-symbols-outlined">chevron_left</span></button>
         
-            <div class="relative h-full m-5 flex justify-center items-center slider-item">
-              <div class="image-container flex justify-center items-center relative overflow-hidden rounded-lg border-sm w-[50vh]">
-                <img class="cursor-pointer w-full" :src="currentImage.full">
+            <div class="relative h-full m-5 flex justify-center items-center slider-item">               
+              <div class="image-container flex justify-center items-center relative overflow-hidden rounded-lg border-sm w-[80vh]">
+                <img class="cursor-pointer w-full" :src="currentImage.url">
               </div>
+              
               <div class="absolute bottom-0 my-4 font-medium text-xs text-slate-300">{{ currentImage.name }}</div>
             </div>
     
-            <button @click="next"><span class="material-symbols-outlined">chevron_right</span></button>
+            <button class="button" @click="next" @mouseenter="pauseAutoSlide" @mouseleave="resumeAutoSlide"><span class="material-symbols-outlined">chevron_right</span></button>
         </section>
 
         <div class="indicators flex gap-2 justify-center items-center ">
@@ -90,6 +126,13 @@ const goToCurrentSlide = (index) => {
   color: #888;
 }
 .active{
-    color: brown;
+    color: rgb(10, 157, 255);
+}
+.image-slider{
+    border-radius: 10px;
+    padding: 30px 15px 50px 15px;
+}
+.button{
+    background-color: rgb(229, 244, 254);
 }
 </style>
